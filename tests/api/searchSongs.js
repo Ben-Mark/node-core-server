1de5111a01ed61cbf7cbce500cbf37642a29b216
benmark <reactivefames@gmail.com>
2020-06-13 00:10:09 +0300

    init

--- tests/api/searchSongs.js
+++ tests/api/searchSongs.js
@@ -1,18 +1,18 @@
 import test from 'ava';
+
 const {chai} = require('../baseApiTest');
 
 const server = require('./../../app');
 
 test("API: search youtube song with likes percentages", async t => {
 
-   return await chai.request(server)
+    return await chai.request(server)
         .post('/api/search')
         .send({
             searchWord: "2pac%202020%20remix"
         })
         .then((result) => {
-           // console.log(result);
-            t.assert(true)
+            t.assert(result.body.leading === 10)
         })
         .catch(err => {
             console.error(err);
