dccfd284af822af5b10793aee54a431163f95c53
benmark <reactivefames@gmail.com>
2020-06-20 19:28:32 +0300

    before express migration

--- app.js
+++ app.js
@@ -5,45 +5,33 @@ if(process.env.NODE_ENV){
 }
 
 
-const createError = require('http-errors'),
-    express = require('express'),
-    logger = require('morgan'),
-    routes = require('./lib/routes'),
-    bodyParser = require('body-parser'),
-    {orm} = require('./lib/orm'),
-    app = express(),
-    cors = require('cors');
-
-console.log("Server started, listening on port: " + process.env.PORT);
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(cors());
-app.use('/', routes);
+let http = require("http");
+let https = require("https");
+let url = require("url");
+let fs = require("fs");
+let log = require('rf-commons').logger(module);
+let WebSocketServer = require('./lib/websocket');
+require('dotenv').config();
 
 
-app.set('orm', orm);
-//init youtubedl texts and faqs
-// orm.initData().then(store => app.set('store', store));
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
-
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.send(err.message);
-    console.log(err);
+const httpServer = http.createServer(function (request, response) {
+    log.info("Received secure request for " + request.url);
+    response.write("Reactive frames, signaling server");
+    response.end();
 });
 
 
-process.on('uncaughtException', function (err) {
-    console.log(" UNCAUGHT EXCEPTION ");
-    console.log("[Inside 'uncaughtException' event] " + err.stack || err.message);
+// Spin up the HTTPS server on the port assigned to this sample.
+// This will be turned into a WebSocket port very shortly.
+httpServer.listen(process.env.PORT, function () {
+    log.info(`Server is listening on port ${process.env.PORT}`);
 });
 
 
-module.exports = app;
+// Create the WebSocket server by converting the HTTPS server into one.
+new WebSocketServer({
+    httpServer: httpServer,
+    autoAcceptConnections: false
+});
+
+
--- tests/api/searchSongs.js
+++ tests/api/searchSongs.js
@@ -1,22 +1,22 @@
-import test from 'ava';
-
-const {chai} = require('../baseApiTest');
-
-const server = require('./../../app');
-
-test("API: search youtube song with likes percentages", async t => {
-
-    return await chai.request(server)
-        .post('/api/search')
-        .send({
-            searchWord: "2pac%202020%20remix"
-        })
-        .then((result) => {
-            t.assert(result.body.length === 10)
-        })
-        .catch(err => {
-            console.error(err);
-        })
-
-
-});
+// import test from 'ava';
+//
+// const {chai} = require('../baseApiTest');
+//
+// const server = require('../../app1');
+//
+// test("API: search youtube song with likes percentages", async t => {
+//
+//     return await chai.request(server)
+//         .post('/api/search')
+//         .send({
+//             searchWord: "2pac%202020%20remix"
+//         })
+//         .then((result) => {
+//             t.assert(result.body.length === 10)
+//         })
+//         .catch(err => {
+//             console.error(err);
+//         })
+//
+//
+// });
