cc54edf472c4f930799efe411af0843250298c39
benmark <reactivefames@gmail.com>
2020-09-23 09:05:00 +0300

    init

--- app.js
+++ app.js
@@ -1,37 +1,23 @@
-if(process.env.NODE_ENV){
-    require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
-}else{
-    require('dotenv').config();
-}
+const express = require('express'),
+    {createServer} = require("http"),
+    expressWs = require('express-ws'),
+    app = express(),
+    logger = require('./lib/utils/Logger')(module),
+    WebSocketSocketHandler = require('./lib/http/websocket/webSocketHandler');
 
+process.env.NODE_ENV
+    ? require('dotenv').config({path: `.env.${process.env.NODE_ENV}`})
+    : require('dotenv').config()
 
-let http = require("http");
-let https = require("https");
-let url = require("url");
-let fs = require("fs");
-let log = require('rf-commons').logger(module);
-let WebSocketServer = require('./lib/websocket');
-require('dotenv').config();
+webSocketSocketHandler = new WebSocketSocketHandler();
+app.set('wsHandler',webSocketSocketHandler);
 
+const server = createServer(app);
 
-const httpServer = http.createServer(function (request, response) {
-    log.info("Received secure request for " + request.url);
-    response.write("Reactive frames, signaling server");
-    response.end();
+server.listen(process.env.PORT, () =>{
+    logger.info(`Server running on port: ${process.env.PORT}`)
 });
+expressWs(app, server)
 
-
-// Spin up the HTTPS server on the port assigned to this sample.
-// This will be turned into a WebSocket port very shortly.
-httpServer.listen(process.env.PORT, function () {
-    log.info(`Server is listening on port ${process.env.PORT}`);
-});
-
-
-// Create the WebSocket server by converting the HTTPS server into one.
-new WebSocketServer({
-    httpServer: httpServer,
-    autoAcceptConnections: false
-});
-
-
+app.use(express.json({extended: false}));
+app.use("/", require("./lib/http"));
